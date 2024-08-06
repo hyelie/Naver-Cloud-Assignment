c7e71ee9d01ad0ae345996a42980c45a77888f8a
hyeil.jeong <hyeil.jeong@navercorp.com>
2024-08-06 17:32:26 +0900

    feat: Make connection with DB

--- src/main/java/assignment/simpleboard/PingController.java
+++ src/main/java/assignment/simpleboard/PingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
-    @GetMapping("/")
+    @GetMapping("/ping")
     public ResponseEntity<Object> ping() {
         return ResponseEntity.ok("OK");
     }
